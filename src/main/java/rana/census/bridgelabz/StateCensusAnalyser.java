708e9f17bd02937fb9b5f7eadb7d5e8af35f1269
Mr. Avinash Rana <9771971429@users.noreply.github.com>
2020-04-10 10:58:23 +0530

    AVINASH :: [ADDED] -> program to match number of records

--- src/main/java/rana/census/bridgelabz/StateCensusAnalyser.java
+++ src/main/java/rana/census/bridgelabz/StateCensusAnalyser.java
@@ -1,5 +1,6 @@
 package rana.census.bridgelabz;
 
+import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvException;
@@ -7,19 +8,24 @@ import com.opencsv.exceptions.CsvException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class StateCensusAnalyser {
 
-    public StateCensusAnalyser() throws IOException, CsvException {
+    public int stateCensusAnalyser_data() throws IOException, CsvException {
+        int count=1;
         try(
                 BufferedReader bufferedReader = new BufferedReader(new FileReader("IndiaStateCensusData.csv"));
+                CSVReader csvReader = new CSVReader(bufferedReader);
         ){
             CsvToBean<CSVdata> csvToBean = new CsvToBeanBuilder<CSVdata>(bufferedReader)
                     .withType(CSVdata.class)
                     .build();
 
             Iterator<CSVdata> csvdataIterator = csvToBean.iterator();
+
             while (csvdataIterator.hasNext()){
                 CSVdata csvdata = csvdataIterator.next();
                 System.out.println("State :: " + csvdata.getState());
@@ -27,7 +33,9 @@ public class StateCensusAnalyser {
                 System.out.println("State :: " + csvdata.getAreaInSqKm());
                 System.out.println("State :: " + csvdata.getDensityPerSqKm());
                 System.out.println("*****************************************");
+                count++;
             }
         }
+        return count;
     }
 }
--- src/test/java/rana/census/bridgelabz/StateCensusAnalyserTest.java
+++ src/test/java/rana/census/bridgelabz/StateCensusAnalyserTest.java
@@ -1,13 +1,34 @@
 package rana.census.bridgelabz;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.junit.Test;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class StateCensusAnalyserTest {
-    @Test
-    public void iterator(){
+    StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
 
+    public static List<String[]> records;
+    public StateCensusAnalyserTest() throws IOException, CsvException {
+        try(
+                BufferedReader bufferedReader=new BufferedReader(new FileReader("IndiaStateCensusData.csv"));
+                CSVReader csvReader=new CSVReader(bufferedReader);
+                ){
+             records= csvReader.readAll();
+        }
     }
 
+    @Test
+    public void iterator() throws IOException, CsvException {
+        assertEquals(records.size(), stateCensusAnalyser.stateCensusAnalyser_data());
+    }
 }
