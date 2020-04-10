17d4d896821d88b7ffa8d4dedb01f21d48344fcc
Mr. Avinash Rana <9771971429@users.noreply.github.com>
2020-04-10 09:09:25 +0530

    AVINASH : [ADDED] -> welcome message

---
+++ src/main/java/rana/census/bridgelabz/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package rana.census.bridgelabz;
+
+public class CensusAnalyser {
+    public static void main(String[] args){
+        System.out.println("-- Welcome To Census Analyser --");
+    }
+}
